52c6db41b6c1a3570ba09dabd323287824aa7a35
AntonTupikin <antontupikin@icloud.com>
2025-08-12 13:13:54 +0500

    Second commit. Working project

--- src/main/java/com/example/multidatasoure/MultidatasoureApplication.java
+++ src/main/java/com/example/multidatasoure/MultidatasoureApplication.java
@@ -2,7 +2,11 @@ package com.example.multidatasoure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class MultidatasoureApplication {
     public static void main(String[] args) {
